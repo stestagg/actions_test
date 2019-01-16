2f4259ed9bccba356860b3ada61ade576da6fc8d
Steve Stagg <stestagg@gmail.com>
2019-01-16 23:03:46 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,13 @@
 
 apk add tree
 tree -a
+
+TOKEN_URL=$(git remote get-url origin | sed s/github.com/$GITHUB_TOKEN@github.com/)
+git remote set-url origin $TOKEN_URL
+
+git checkout test
+echo HI > a.txt
+git add a.txt
+git commit -m auto test
+
+git push
